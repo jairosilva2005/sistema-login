22a3ad16b6e5cebe9b15478680808478cac42560
Jairo Silva <70125199+jairosilva2005@users.noreply.github.com>
2021-03-23 10:41:13 -0300

    Update connect_db.php

--- connect/connect_db.php
+++ connect/connect_db.php
@@ -1,7 +1,7 @@
 <?php
 
     try {
-        $Connect = new PDO("mysql:host=localhost;dbname=loginpdo", "root", "jairohds");
+        $Connect = new PDO("mysql:host=localhost;dbname=loginpdo", "root", ""); // Coloque a senha do seu MySQL
     } catch(PDOException $Error) {
         echo "Database error: " . $Error -> getMessage();
     } catch(Exception $Error) {
